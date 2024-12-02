7251611cb6227f15a2b4a1a10c22912b844674ac
Giulia Moretti <Giulia Moretti>
2024-12-02 11:48:45 +0100

    Aggiunta interfaccia grafica, aggiunto toggle switch, sistemazione bottoni, sistemazione file

--- src/it/edu/iisgubbio/lovelace/Interfaccia.java
+++ src/it/edu/iisgubbio/lovelace/Interfaccia.java
@@ -1,34 +1,113 @@
 package it.edu.iisgubbio.lovelace;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import it.edu.iisgubbio.lovelace.dynamicEffects.*;
 
 public class Interfaccia extends Application{
-	Pane quadro =new Pane();
-	Button bStart=new Button("Start");
-	Button bSettings=new Button("Settings");
-	Label eTitolo=new Label("The Lovelace Game");
-	final int ALTEZZA_QUADRO=700;
-	final int LARGHEZZA_QUADRO=800;
+	Pane areaGioco = new Pane();
+	Label eTitolo = new Label("The Lovelace Game");
+	Label eSottoTitolo = new Label("Aiuta Ada a costruire il suo algoritmo!");
+	Button bInizio = new Button("Inizio");
+	Button bImpostazioni = new Button("Impostazioni");
+	Button bEsci = new Button("Esci");
+	Button bTornaAlMenu = new Button("🏠");
+	Group home = new Group();
+	
+	
+	final int LARGHEZZA_AREA_GIOCO = 900;
+	final int ALTEZZA_AREA_GIOCO = 700;
 
-	public void start(Stage finestra) {
-		quadro.getChildren().addAll(eTitolo, bStart, bSettings );
-		eTitolo.setLayoutX(LARGHEZZA_QUADRO/2-50);
-		eTitolo.setLayoutY(ALTEZZA_QUADRO-600);
-		bStart.setLayoutY((ALTEZZA_QUADRO/2)+25);
-		bStart.setLayoutX(LARGHEZZA_QUADRO/2-50);
-		Scene scena = new Scene(quadro,LARGHEZZA_QUADRO,ALTEZZA_QUADRO);
+
+
+	public void start(Stage finestra) throws Exception {
+		
+		home.getChildren().add(eTitolo);
+		home.getChildren().add(eSottoTitolo);
+		home.getChildren().add(bInizio);
+		home.getChildren().add(bImpostazioni);
+		home.getChildren().add(bEsci);
+		
+		areaGioco.getChildren().add(home);
+		
+		(new FadeIn(home, 2000, 30)).start();
+		
+		eTitolo.setLayoutX(340);
+		eTitolo.setLayoutY(100);
+		
+		eSottoTitolo.setLayoutX(342);
+		eSottoTitolo.setLayoutY(150);
+		
+		bInizio.setLayoutX(342);
+		bInizio.setLayoutY(250);
+		
+		bImpostazioni.setLayoutX(342);
+		bImpostazioni.setLayoutY(310);
+		
+		bEsci.setLayoutX(342);
+		bEsci.setLayoutY(373);
+		
+		bInizio.setPrefWidth(200);
+		bImpostazioni.setPrefWidth(200);
+		bEsci.setPrefWidth(200);
+		
+		bImpostazioni.setOnAction(e-> impostazioni());
+		bEsci.setOnAction(e-> esci());
+		
+		Image icon = new Image(getClass().getResourceAsStream("immagini/IMG_2263.jpeg"));
+	    finestra.getIcons().add(icon);
+		
+		
+		eTitolo.setId("titolo");
+		areaGioco.setId("paneSfondo");
+		
+
+		Scene scena = new Scene(areaGioco,LARGHEZZA_AREA_GIOCO,ALTEZZA_AREA_GIOCO);
+		scena.getStylesheets().add("it/edu/iisgubbio/lovelace/foglio.css");
+		
+
+		finestra.setTitle("The Lovalace Game!");
+		finestra.setResizable(false);
 		finestra.setScene(scena);
 		finestra.show();
 	}
-	public static void main(String[] args) {
-		launch(args);	
+	
+	public void impostazioni() {
+		Group effetto = new Group();
+		Label lingua = new Label("Lingua:");
+		lingua.setId("testo");
+		ToggleSwitch button = new ToggleSwitch();
+		effetto.getChildren().add(bTornaAlMenu);
+		effetto.getChildren().add(button);
+		(new FadeIn(effetto, 2000)).start();
+		areaGioco.getChildren().clear();
+		areaGioco.setId("paneSfondo");
+		areaGioco.getChildren().add(effetto);
+		areaGioco.getChildren().add(lingua);
+		button.setLayoutX(381);
+		button.setLayoutY(373);
+		lingua.setLayoutX(390);
+		lingua.setLayoutY(355);
+		bTornaAlMenu.setLayoutX(10);
+		bTornaAlMenu.setLayoutY(7);
+		
+	}
+	
+	public void esci() {
+		Platform.exit();
+	}
 
+	public static void main(String args[]){
+		launch();
 	}
 }
---
+++ src/it/edu/iisgubbio/lovelace/ToggleSwitch.java
@@ -0,0 +1,63 @@
+package it.edu.iisgubbio.lovelace;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+
+public class ToggleSwitch extends HBox {
+	
+	private final Label label = new Label();
+	private final Button button = new Button();
+	
+	private SimpleBooleanProperty switchedOn = new SimpleBooleanProperty(false);
+	public SimpleBooleanProperty switchOnProperty() { return switchedOn; }
+	
+	private void init() {
+		
+		label.setText("ITALIANO");
+		
+		getChildren().addAll(label, button);	
+		button.setOnAction((e) -> {
+			switchedOn.set(!switchedOn.get());
+		});
+		label.setOnMouseClicked((e) -> {
+			switchedOn.set(!switchedOn.get());
+		});
+		setStyle();
+		bindProperties();
+	}
+	
+	private void setStyle() {
+		//Default Width
+		setWidth(130);
+		label.setAlignment(Pos.CENTER);
+		setStyle("-fx-background-color: a374f5; -fx-text-fill:black; -fx-background-radius: 10;");
+		setAlignment(Pos.CENTER_LEFT);
+	}
+	
+	private void bindProperties() {
+		label.prefWidthProperty().bind(widthProperty().divide(2));
+		label.prefHeightProperty().bind(heightProperty());
+		button.prefWidthProperty().bind(widthProperty().divide(2));
+		button.prefHeightProperty().bind(heightProperty());
+	}
+	
+	public ToggleSwitch() {
+		init();
+		switchedOn.addListener((a,b,c) -> {
+			if (c) {
+                		label.setText("INGLESE");
+                		setStyle("-fx-background-color:  #a374f5; -fx-background-radius: 10;");
+                		label.toFront();
+            		}
+            		else {
+            			label.setText("ITALIANO");
+        			setStyle("-fx-background-color:  #a374f5; -fx-background-radius: 10;");
+                		button.toFront();
+            		}
+		});
+	}
+}
